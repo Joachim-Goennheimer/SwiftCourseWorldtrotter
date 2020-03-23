baf442bbc589f78db73ab9fe655b15c3bdf180e0
Joachim Goennheimer <Joachim.goennheimer@ibm.com>
2020-03-23 08:13:22 +0100

    commit before create view programmatically

---
+++ WorldTrotter/MapViewController.swift
@@ -0,0 +1,15 @@
+//
+//  MapViewController.swift
+//  WorldTrotter
+//
+//  Created by Joachim Goennheimer on 23.03.20.
+//  Copyright © 2020 Joachim Goennheimer. All rights reserved.
+//
+
+//import Foundation
+
+import UIKit
+
+class MapViewController: UIViewController {
+    
+}
